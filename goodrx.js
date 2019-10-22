8b9ae86af93aa141c294ccfc3e8a12553f014066
Dan Valinotti <dvalinotti@galaxe.com>
2019-10-22 09:52:28 -0400

    Release 1.2.0 - changes to top-4 on InsideRx, addition of unc_price

--- goodrx.js
+++ goodrx.js
@@ -1,20 +1,22 @@
 // GOOD RX //
-const {Client} = require('/opt/node_modules/pg');
+const {
+    Client
+} = require('/opt/node_modules/pg');
 var rp = require('/opt/node_modules/request-promise');
 var db_host = process.env.DB_HOST;
 var reg = process.env.REGION;
 
-function DateFunction(){
+function DateFunction() {
     var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var dateTime = date+' '+time;
+    var dateTime = date + ' ' + time;
     return dateTime;
 }
 
-function comparePrices(a,b){
-    if(a.price === null) return 1;
-    if(b.price === null) return -1;
+function comparePrices(a, b) {
+    if (a.price === null) return 1;
+    if (b.price === null) return -1;
     if (a.price > b.price) return 1;
     if (b.price >= a.price) return -1;
 }
@@ -25,99 +27,99 @@ async function sleep(ms) {
 }
 
 async function getGoodRxPrices(url, options, drugId, query, values, client) {
-    return await rp(options).then(async function(response) {
+    return await rp(options).then(async function (response) {
         var data = response;
-        
+
         if (data != undefined) {
             var results = data.results;
             console.log(results);
             var CVSPrice = {};
-            CVSPrice.price = null ;
-            CVSPrice.pharmacy=null;
+            CVSPrice.price = null;
+            CVSPrice.pharmacy = null;
             CVSPrice.rank = 0;
             var WalmartPrice = {};
-            WalmartPrice.price =  null;
-            WalmartPrice.pharmacy=null;
+            WalmartPrice.price = null;
+            WalmartPrice.pharmacy = null;
             WalmartPrice.rank = 0;
             var WalgreenPrice = {};
-            WalgreenPrice.price =  null;
-            WalgreenPrice.pharmacy=null;
+            WalgreenPrice.price = null;
+            WalgreenPrice.pharmacy = null;
             WalgreenPrice.rank = 0;
             var KrogerPrice = {};
-            KrogerPrice.price =  null ;
-            KrogerPrice.pharmacy =null;
+            KrogerPrice.price = null;
+            KrogerPrice.pharmacy = null;
             KrogerPrice.rank = 0;
             var OtherPrice = {};
-            OtherPrice.price =  null ;
-            OtherPrice.pharmacy =null;
+            OtherPrice.price = null;
+            OtherPrice.pharmacy = null;
             OtherPrice.rank = 0;
-            
-            results.forEach(function(value){
-                if(value!= null){
-                   
-                    if(value.pharmacy.name.toUpperCase().includes("CVS")){
-                        
-                        if(CVSPrice.price == null || CVSPrice.price > parseFloat(value.prices[0].price)){
-                            CVSPrice.price =  parseFloat(value.prices[0].price);
-                            CVSPrice.pharmacy=value.pharmacy.name;
+
+            results.forEach(function (value) {
+                if (value != null) {
+
+                    if (value.pharmacy.name.toUpperCase().includes("CVS")) {
+
+                        if (CVSPrice.price == null || CVSPrice.price > parseFloat(value.prices[0].price)) {
+                            CVSPrice.price = parseFloat(value.prices[0].price);
+                            CVSPrice.pharmacy = value.pharmacy.name;
                         }
-                       
-                    }else if(value.pharmacy.name.toUpperCase().includes("WALMART")){
-                        if(WalmartPrice.price == null ||WalmartPrice.price > parseFloat(value.prices[0].price)){
-                            WalmartPrice.price =  parseFloat(value.prices[0].price);
-                            WalmartPrice.pharmacy=value.pharmacy.name;
+
+                    } else if (value.pharmacy.name.toUpperCase().includes("WALMART")) {
+                        if (WalmartPrice.price == null || WalmartPrice.price > parseFloat(value.prices[0].price)) {
+                            WalmartPrice.price = parseFloat(value.prices[0].price);
+                            WalmartPrice.pharmacy = value.pharmacy.name;
                         }
-                      
-                    }else if(value.pharmacy.name.toUpperCase().includes("WALGREENS")){
-                        if(WalgreenPrice.price == null ||WalgreenPrice.price > parseFloat(value.prices[0].price)){
-                            WalgreenPrice.price =  parseFloat(value.prices[0].price);
-                            WalgreenPrice.pharmacy=value.pharmacy.name;
+
+                    } else if (value.pharmacy.name.toUpperCase().includes("WALGREENS")) {
+                        if (WalgreenPrice.price == null || WalgreenPrice.price > parseFloat(value.prices[0].price)) {
+                            WalgreenPrice.price = parseFloat(value.prices[0].price);
+                            WalgreenPrice.pharmacy = value.pharmacy.name;
                         }
-                       
-                    }else if(value.pharmacy.name.toUpperCase().includes("KROGER")){
-                        if(KrogerPrice.price == null ||KrogerPrice.price > parseFloat(value.prices[0].price)){
-                            KrogerPrice.price =  parseFloat(value.prices[0].price);
-                            KrogerPrice.pharmacy=value.pharmacy.name;
+
+                    } else if (value.pharmacy.name.toUpperCase().includes("KROGER")) {
+                        if (KrogerPrice.price == null || KrogerPrice.price > parseFloat(value.prices[0].price)) {
+                            KrogerPrice.price = parseFloat(value.prices[0].price);
+                            KrogerPrice.pharmacy = value.pharmacy.name;
                         }
-                       
-                    }else {
-                        if(OtherPrice.price == null || OtherPrice.price > parseFloat(value.prices[0].price)){
-                            OtherPrice.price =  parseFloat(value.prices[0].price);
-                            OtherPrice.pharmacy=value.pharmacy.name;
+
+                    } else {
+                        if (OtherPrice.price == null || OtherPrice.price > parseFloat(value.prices[0].price)) {
+                            OtherPrice.price = parseFloat(value.prices[0].price);
+                            OtherPrice.pharmacy = value.pharmacy.name;
                         }
-                        
+
                     }
-                   
+
                 }
             });
-            var pricesArr = [WalgreenPrice,WalmartPrice,CVSPrice,OtherPrice, KrogerPrice];
+            var pricesArr = [WalgreenPrice, WalmartPrice, CVSPrice, OtherPrice, KrogerPrice];
             console.log(pricesArr)
             pricesArr.sort(comparePrices)
-           
+
             pricesArr[0].rank = 0;
             pricesArr[1].rank = 1;
             pricesArr[2].rank = 2;
             pricesArr[3].rank = 3;
             pricesArr[4].rank = 4;
-            pricesArr.forEach(async function (price){
+            pricesArr.forEach(async function (price) {
                 const pricingData = {
                     //id : "",
-                    average_price : 0.0,
-                    createdat : DateFunction(),  
-                    difference : 0.0,
-                    lowest_market_price : 0.0,
-                    drug_details_id : drugId,
-                    pharmacy : price.pharmacy,
-                    price : price.price,
-                    program_id : 6,
-                    recommended_price : 0.0,  
-                    rank:price.rank,
+                    average_price: 0.0,
+                    createdat: DateFunction(),
+                    difference: 0.0,
+                    lowest_market_price: 0.0,
+                    drug_details_id: drugId,
+                    pharmacy: price.pharmacy,
+                    price: price.price,
+                    program_id: 6,
+                    recommended_price: 0.0,
+                    rank: price.rank,
                 };
-                
+
                 // console.log("pricingData"+pricingData);
-                
+
                 console.log("DRUG_DETAILS_ID: " + pricingData.drug_details_id);
-                
+
                 query = 'INSERT INTO public_price(average_price, createdat, difference, drug_details_id, lowest_market_price, pharmacy, price, program_id, recommended_price,rank,unc_price) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11) RETURNING *;'
                 values = [
                     pricingData.average_price,
@@ -132,9 +134,9 @@ async function getGoodRxPrices(url, options, drugId, query, values, client) {
                     pricingData.rank,
                     null
                 ];
-                
+
                 // console.log(values[0]);
-                
+
                 // PLACE PRICE RESULT INTO PUBLIC_PRICE TABLE
                 await client.query(query, values)
                     .then((response) => {
@@ -144,46 +146,46 @@ async function getGoodRxPrices(url, options, drugId, query, values, client) {
                     .catch((error) => console.log(error));
             });
         }
-    }).catch(function(error) { 
+    }).catch(function (error) {
         console.log("Failure: " + drugId);
         console.log("URL: " + url)
-        return "ERROR";    
+        return "ERROR";
     })
 }
 
-exports.handler = async function(event) {
+exports.handler = async function (event) {
     var drugId = "";
     var url = "";
     var query2 = "";
     var values = "";
-    
+
     // CONNECT TO POSTGRES DB
     const connectionString = db_host;
     const client = new Client({
         connectionString: connectionString
     });
     client.connect();
-    
+
     let drugList = [];
-    
+
     var response = await client.query("SELECT drug_id FROM shuffle_drugs where flag = 'pending' and region = '" + reg + "';");
     for (var i = 0; i < response.rows.length; i++) {
         for (var j = 0; j < response.rows[i].drug_id.length; j++) {
             drugList.push(response.rows[i].drug_id[j]);
         }
     }
-    
+
     var a = 0;
     let len = drugList.length;
-        // console.log(len);
-        
+    // console.log(len);
+
     let priceCount = 0;
-    
+
     for (var k = 0; k < len; k++) {
         // console.log(drugList[k]);
         var req = await client.query("SELECT * FROM drug_request where program_id = 6 and good_rx_id is not null and drug_name is not null and latitude is not null and longitude is not null and quantity is not null and drug_id :: int = " + drugList[k]);
         // console.log(req.rows.length);
-        if(req.rows.length >= 1) {
+        if (req.rows.length >= 1) {
             drugId = req.rows[0].drug_id;
             // console.log("CHECKING:::" + drugId);
             var drugName = req.rows[0].drug_name;
@@ -192,9 +194,9 @@ exports.handler = async function(event) {
             var brand = req.rows[0].brand_indicator;
             var quantity = req.rows[0].quantity;
             var grxId = req.rows[0].good_rx_id;
-            
+
             url = `https://goodrx.com/api/v4/drugs/${grxId}/prices?location=${longitude},${latitude}&location_type=LAT_LNG_GEO_IP&quantity=${quantity}`;
-            
+
             const options = {
                 url: url,
                 method: 'GET',
@@ -206,28 +208,29 @@ exports.handler = async function(event) {
                     "User-Agent": "PostmanRuntime/7.16.3"
                 }
             };
-            
+
             if (priceCount % 30 === 0) {
                 console.log("About to be blocked, waiting...")
                 await sleep(60000);
                 console.log("Waited 1min")
             }
-            
+
             try {
                 // MAKE GOODRX API REQUEST
                 var res;
                 res = await getGoodRxPrices(url, options, drugId, query2, values, client);
-                
+
                 priceCount = priceCount + 1;
             } catch (error) {
                 console.log(error);
             }
-        } 
-        
+        }
+
         // var query3 = 'SELECT * FROM public_price WHERE program_id = 6';
         // await client.query(query3).then((response) => {
         //     console.log(response.rows.length)
         // }).catch((error) => console.log(error));
-    // }
-} console.log(priceCount);
+        // }
+    }
+    console.log(priceCount);
 }
--- insiderx.js
+++ insiderx.js
@@ -1,13 +1,22 @@
-var rp = require(process.env.NODE_ENV === 'dev' ? 'request-promise' : '/opt/node_modules/request-promise');
+var rp = require('/opt/node_modules/request-promise');
 const {
     Pool,
     Client
-} = require(process.env.NODE_ENV === 'dev' ? 'pg' : '/opt/node_modules/pg');
+} = require('/opt/node_modules/pg');
 const request = require("request");
 var db_host = process.env.DB_HOST;
 var reg = process.env.REGION;
 
+
 const connectionString = db_host;
+
+function comparePrices(a, b) {
+    if (a.lowestPrice === null) return 1;
+    if (b.lowestPrice === null) return -1;
+    if (a.lowestPrice > b.lowestPrice) return 1;
+    if (b.lowestPrice >= a.lowestPrice) return -1;
+}
+
 
 function DateFunction() {
     var today = new Date();
@@ -33,6 +42,8 @@ let pricingData1 = {
     price: 0,
     program_id: 0,
     recommended_price: 0,
+    rank: 0,
+    uncPrice: null
 }
 
 //let results =""
@@ -54,11 +65,11 @@ exports.myhandler = async function abc() {
     }
     var a = 0;
     len = listDrugs.length;
-    console.log(len);
+    console.log(listDrugs);
     for (var k = 0; k < len; k++) {
         var drugUrlList = await client.query("SELECT * FROM drug_request where program_id = 0 and drug_name is not null and latitude is not null and longitude is not null and quantity is not null and drug_id :: int = " + listDrugs[k]);
 
-        if (drugUrlList.rows.length == 1) {
+        if (drugUrlList.rows.length > 0) {
             //console.log("********************"+a);
 
             DrugId = parseInt(drugUrlList.rows[0].drug_id);
@@ -85,41 +96,178 @@ exports.myhandler = async function abc() {
                 url: "https://insiderx.com/request/pharmacies",
                 headers: {
                     // "Referer":"https://www.wellrx.com/prescriptions/"+encodeURI(wrxbody.drugname)+"/08823",
-                    "Cookie": "_gcl_au=1.1.1639244140.1555443999; _fbp=fb.1.1555443999440.65320427; _ga=GA1.2.711100002.1555444000; _gid=GA1.2.317294123.1555444000; _hjIncludedInSample=1; _csrf=Z3iefjYKIjIUIEXBJgTix0BY; _gat_UA-113293481-1=1; geocoords=40.7473758%2C-74.05057520000003; AWSALB=6NBPPYHYpRwHG5ONO7yvFP6fzmSCfiDRLUr3FCKprscG4ld2CKg2lU+ZRCxhxrTF55clcMF7APSLyeZBhLeH2pv/9pzCIWt8u9lcfJfF8La8Z/eIpABRoF3orpJj",
+                    "Cookie": "_gcl_au=1.1.923916117.1571676777; _fbp=fb.1.1571676776869.2055137922; _ga=GA1.2.930772864.1571676778; _gid=GA1.2.1882699394.1571676778; _gat_UA-113293481-1=1; _hjid=d6e7565b-1525-4271-8198-042e450e45ac; _hjIncludedInSample=1; geocoords=40.7350747%2C-74.17390569999998; AWSALB=mSNItEQ6fXmxXUsxt5mlUriIXhzEHmbChrsjCmQCdVdp42tXWv07gpOMfIQjeOlkAmbeYVCzgbur0wS6jc3a92h9ZKJJb9cNCF7qpmn5FKV9PH3VfDW/CsYPWDt2",
                     "X-Requested-With": "XMLHttpRequest",
                     "Accept": "application/json",
                     "csrf-token": "Hi6yGXfg-vppErZsd2KXvKmH9LxjPBNJeK48",
 
                 }
             }
+
             try {
 
                 await rp(options).then(async function (response) {
                     var jsondata1 = response;
-                    var lowestPrice = jsondata1.prices[0].price;
-                    var lowestPharmacy = jsondata1.prices[0].pharmacy.name;
+                    var CVSPrice = {};
+                    CVSPrice.price = null;
+                    CVSPrice.pharmacy = null;
+                    CVSPrice.rank = 0;
+                    CVSPrice.uncPrice = null;
+                    CVSPrice.lowestPrice = null;
+                    var WalmartPrice = {};
+                    WalmartPrice.price = null;
+                    WalmartPrice.pharmacy = null;
+                    WalmartPrice.rank = 0;
+                    WalmartPrice.uncPrice = null;
+                    WalmartPrice.lowestPrice = null;
+                    var WalgreenPrice = {};
+                    WalgreenPrice.price = null;
+                    WalgreenPrice.pharmacy = null;
+                    WalgreenPrice.rank = 0;
+                    WalgreenPrice.uncPrice = null;
+                    WalgreenPrice.lowestPrice = null;
+                    var KrogerPrice = {};
+                    KrogerPrice.uncPrice = null;
+                    KrogerPrice.price = null;
+                    KrogerPrice.pharmacy = null;
+                    KrogerPrice.rank = 0;
+                    KrogerPrice.lowestPrice = null;
+                    var OtherPrice = {};
+                    OtherPrice.price = null;
+                    OtherPrice.pharmacy = null;
+                    OtherPrice.rank = 0;
+                    OtherPrice.uncPrice = null;
+                    OtherPrice.lowestPrice = null;
+
                     jsondata1.prices.forEach(function (value) {
                         if (value != null) {
-                            if (lowestPrice > value.price) {
-                                lowestPrice = value.price;
-                                lowestPharmacy = value.pharmacy.name;
+                            if (value.pharmacy.name.toUpperCase().includes("CVS")) {
+
+                                if (CVSPrice.price == null || CVSPrice.lowestPrice > parseFloat(value.price)) {
+                                    CVSPrice.price = parseFloat(value.price);
+                                    CVSPrice.pharmacy = value.pharmacy.name;
+                                    CVSPrice.uncPrice = null;
+                                    CVSPrice.lowestPrice = parseFloat(value.price);
+                                    if (value.uncPrice != undefined && value.uncPrice != null) {
+                                        CVSPrice.uncPrice = parseFloat(value.uncPrice)
+                                    }
+
+
+                                }
+                                if (value.uncPrice != undefined && value.uncPrice != null && CVSPrice.lowestPrice > parseFloat(value.uncPrice)) {
+                                    CVSPrice.price = parseFloat(value.price);
+                                    CVSPrice.pharmacy = value.pharmacy.name;
+                                    CVSPrice.uncPrice = parseFloat(value.uncPrice);
+                                    CVSPrice.lowestPrice = parseFloat(value.uncPrice);
+
+                                }
+                            } else if (value.pharmacy.name.toUpperCase().includes("WAL-MART")) {
+                                if (WalmartPrice.price == null || WalmartPrice.lowestPrice > parseFloat(value.price)) {
+                                    WalmartPrice.price = parseFloat(value.price);
+                                    WalmartPrice.pharmacy = value.pharmacy.name;
+                                    WalmartPrice.uncPrice = null;
+                                    WalmartPrice.lowestPrice = parseFloat(value.price);
+                                    if (value.uncPrice != undefined && value.uncPrice != null) {
+                                        WalmartPrice.uncPrice = parseFloat(value.uncPrice)
+                                    }
+
+
+                                }
+                                if (value.uncPrice != undefined && value.uncPrice != null && WalmartPrice.lowestPrice > parseFloat(value.uncPrice)) {
+                                    WalmartPrice.price = parseFloat(value.price);
+                                    WalmartPrice.pharmacy = value.pharmacy.name;
+                                    WalmartPrice.uncPrice = parseFloat(value.uncPrice);
+                                    WalmartPrice.lowestPrice = parseFloat(value.uncPrice);
+
+                                }
+
+                            } else if (value.pharmacy.name.toUpperCase().includes("WALGREENS")) {
+                                if (WalgreenPrice.lowestPrice == null || WalgreenPrice.lowestPrice > parseFloat(value.price)) {
+                                    WalgreenPrice.price = parseFloat(value.price);
+                                    WalgreenPrice.pharmacy = value.pharmacy.name;
+                                    WalgreenPrice.uncPrice = null;
+                                    WalgreenPrice.lowestPrice = parseFloat(value.price);
+                                    if (value.uncPrice != undefined && value.uncPrice != null) {
+                                        WalgreenPrice.uncPrice = parseFloat(value.uncPrice)
+                                    }
+
+
+                                }
+                                if (value.uncPrice != undefined && value.uncPrice != null && WalgreenPrice.price > parseFloat(value.uncPrice)) {
+                                    WalgreenPrice.price = parseFloat(value.price);
+                                    WalgreenPrice.pharmacy = value.pharmacy.name;
+                                    WalgreenPrice.uncPrice = parseFloat(value.uncPrice);
+                                    WalgreenPrice.lowestPrice = parseFloat(value.uncPrice);
+
+                                }
+                            } else if (value.pharmacy.name.toUpperCase().includes("KROGER")) {
+                                if (KrogerPrice.price == null || KrogerPrice.price > parseFloat(value.price)) {
+                                    KrogerPrice.price = parseFloat(value.price);
+                                    KrogerPrice.pharmacy = value.pharmacy.name;
+                                    KrogerPrice.uncPrice = null;
+                                    KrogerPrice.lowestPrice = parseFloat(value.price);
+                                    if (value.uncPrice != undefined && value.uncPrice != null) {
+                                        KrogerPrice.uncPrice = parseFloat(value.uncPrice)
+                                    }
+
+
+                                }
+                                if (value.uncPrice != undefined && value.uncPrice != null && KrogerPrice.price > parseFloat(value.uncPrice)) {
+                                    KrogerPrice.price = parseFloat(value.price);
+                                    KrogerPrice.pharmacy = value.pharmacy.name;
+                                    KrogerPrice.uncPrice = parseFloat(value.uncPrice);
+                                    KrogerPrice.lowestPrice = parseFloat(value.uncPrice);
+                                }
+                            } else {
+                                if (OtherPrice.price == null || OtherPrice.lowestPrice > parseFloat(value.price)) {
+                                    OtherPrice.price = parseFloat(value.price);
+                                    OtherPrice.pharmacy = value.pharmacy.name;
+                                    OtherPrice.uncPrice = null;
+                                    OtherPrice.lowestPrice = parseFloat(value.price);
+                                    if (value.uncPrice != undefined && value.uncPrice != null) {
+                                        OtherPrice.uncPrice = parseFloat(value.uncPrice)
+
+                                    }
+
+
+                                }
+                                if (value.uncPrice != undefined && value.uncPrice != null && OtherPrice.lowestPrice > parseFloat(value.uncPrice)) {
+                                    OtherPrice.price = parseFloat(value.price);
+                                    OtherPrice.pharmacy = value.pharmacy.name;
+                                    OtherPrice.uncPrice = parseFloat(value.uncPrice);
+                                    OtherPrice.lowestPrice = parseFloat(value.uncPrice);
+                                }
                             }
-                            if (value.uncPrice != undefined && value.uncPrice != null && lowestPrice > value.uncPrice) {
-                                lowestPrice = value.uncPrice;
-                                lowestPharmacy = value.pharmacy.name;
-                            }
+
+
                         }
                     });
-                    pricingData1.price = lowestPrice;
-                    pricingData1.pharmacy = lowestPharmacy;
-                    query2 = 'INSERT INTO public_price(average_price, createdat, difference, drug_details_id, lowest_market_price, pharmacy, price, program_id, recommended_price) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *';
-                    values = [pricingData1.average_price, pricingData1.createdat, pricingData1.difference, DrugId, pricingData1.lowest_market_price, pricingData1.pharmacy, pricingData1.price, pricingData1.program_id, pricingData1.recommended_price];
-                    await client.query(query2, values)
-                        .then(res => {})
-                        .catch(e => {
-                            console.log("errr")
-                        })
 
+                    var pricesArr = [WalgreenPrice, WalmartPrice, CVSPrice, OtherPrice, KrogerPrice];
+                    console.log(pricesArr)
+                    pricesArr.sort(comparePrices)
+
+                    pricesArr[0].rank = 0;
+                    pricesArr[1].rank = 1;
+                    pricesArr[2].rank = 2;
+                    pricesArr[3].rank = 3;
+                    pricesArr[4].rank = 4;
+
+                    // console.log(pricesArr)
+                    pricesArr.forEach(async function (price) {
+                        pricingData1.price = price.price;
+                        pricingData1.pharmacy = price.pharmacy;
+                        pricingData1.rank = price.rank
+                        pricingData1.uncPrice = price.uncPrice
+                        query2 = 'INSERT INTO public_price(average_price, createdat, difference, drug_details_id, lowest_market_price, pharmacy, price, program_id, recommended_price, rank,unc_price) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11) RETURNING *';
+                        values = [pricingData1.average_price, pricingData1.createdat, pricingData1.difference, DrugId, pricingData1.lowest_market_price, pricingData1.pharmacy, pricingData1.price, pricingData1.program_id, pricingData1.recommended_price, pricingData1.rank, pricingData1.uncPrice];
+                        await client.query(query2, values)
+                            .then(res => {})
+                            .catch(e => {
+                                console.log(e)
+                            })
+
+                    });
 
 
 
@@ -128,6 +276,7 @@ exports.myhandler = async function abc() {
                     // Crawling failed or Cheerio choked...
                 });;
 
+
             } catch (e) {}
 
         } else {
@@ -135,5 +284,8 @@ exports.myhandler = async function abc() {
         }
     }
     console.log("good drugs:" + a + "drugid:" + DrugId)
+
 }
+
+
 //exports.myhandler()
