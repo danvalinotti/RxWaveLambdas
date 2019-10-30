3570c84d86ccf7b824b76ede181ec5bd2f620075
Dan Valinotti <dvalinotti@galaxe.com>
2019-10-30 11:11:37 -0400

    automated grx id solution

--- extract_grx_id.js
+++ extract_grx_id.js
@@ -1,146 +1,106 @@
-const fetch = require('node-fetch');
+const fetch = require('fetch-retry');
 const fs = require('fs');
-const zipcodes = require('zipcodes');
-const sleep = require('sleep-async')().Promise;
-const headers = {
-    "User-Agent": "PostmanRuntime/7.18.0",
-    "Accept": "*/*",
-    "Cache-Control": "no-cache",
-    "Host": "www.goodrx.com",
-    "Connection": "keep-alive"
-}
-const referrers = [
-    "https://www.google.com",
-    "https://www.bing.com",
-    "https://www.stackoverflow.com",
-    "https://www.goodrx.com"
-];
-const userAgents = [
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41",
-    "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0)"
-];
+const generateUrl = require('./genUrl.js');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://54.81.21.172:27017/rxwave_testing',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.ObjectId;
+const GrxIdSchema = new Schema({
+    id: ObjectId,
+    drugName: String,
+    drugForm: String,
+    dosageStrength: String,
+    dosageStrengthNum: {type: Number, default: 0},
+    dosageStrengthUnit: {type: String, default: ''},
+    volumeNum: {type: Number, default: 0},
+    volumeUnit: {type: String, default: ''},
+    quantity: Number,
+    gsn: String,
+    ndc: String,
+    goodRxId: String,
+    url: String
+});
+const GrxIdModel = mongoose.model('GrxId', GrxIdSchema);
 
-function getId(drug) {
-    const url = generateUrl(drug);
-    // const userAgent = userAgents[getRandomArbitrary(0, userAgents.length - 1)];
-    // const referrer = referrers[getRandomArbitrary(0, referrers.length - 1)];
-    return new Promise(function(resolve, reject) {
-        fetch(url, {
-            method: 'get',
-            headers: {
-                "Accept": "*/*",
-                "Cache-Control": "no-cache",
-            }
-        }).then((response) => response.text())
-        .then((text) => resolve(extractId(text)))
-        .catch((error) => reject(error));
-    });
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
-
 
 async function test() {
-    let drugs = JSON.parse(fs.readFileSync('drugs.json'));
-    let ids = [];
+    let drugs = JSON.parse(fs.readFileSync('full_drug_list.json'));
+    fs.truncateSync('grx_ids.json');
+    console.log('id file cleared');
+    // let ids = [];
 
     // let promiseChain = Promise.resolve();
     for (let drug of drugs) {
-        const url = generateUrl(drug);
-        const userAgent = userAgents[getRandomArbitrary(0, userAgents.length - 1)];
-        const referrer = referrers[getRandomArbitrary(0, referrers.length - 1)];
-
-        let id = await fetch(url, {
-            method: 'get',
-	    headers: {
-		"Connection": "keep-alive",
-		"host": "www.goodrx.com",
-		"Referer": "https://www.goodrx.com",
-		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0"
-	    }
-        }).then((response) => response.text())
-        .then((text) => extractId(text))
-        .catch((error) => console.log(error))
-        console.log(id);
-
-	setTimeout(() => {
-	    ids.push(id)
-	}, 5000);
-        
-        // promiseChain = promiseChain.then(waitForResponse(url));
-
-        // fetches.push(setTimeout(()=>{}, 5000))
+        const res = await fetchId(drug);
+        let model = new GrxIdModel(res);
+        // console.log(model);
+        model.save(function(err, res) {
+            console.log(`Error: ${err}`);
+            // console.log(res);
+            if (err) {
+                return console.log(err);
+            } else {
+                return console.log(`Saved '${model.drugName}' to DB`)
+            }
+        });
     }
 
-    // await Promise.all(drugs.reduce(async (promise, drug) => {
-    //     try {
-    //         const id = await getDrugId(drug);
-    //         console.log('sleep 5 ' + id)
-    //         setTimeout(function() {
-    //             console.log(id);
-    //             ids.push(id);
-    //         }, 5000);
-    //     } catch (err) {
-    //         console.log(err);
-    //     }
-    // }));
-    // Promise.all(fetches).then(function() {
-        console.log("NUM IDS: " + ids.length)
-    // })
-
+    // console.log("NUM IDS: " + ids.length);
+    // const writeText = JSON.stringify(ids);
+    // fs.writeFile('grx_ids.json', writeText, function(err, result) {
+    //     if (err) console.log(err);
+    //     console.log("Write complete");
+    // });
+    // console.log(write);
 }
 
-function generateUrl(drug) {
-    const base = "https://www.goodrx.com/";
-    let url = base + drug.drugName.toLowerCase().split(" ")[0];
-    let label_override = "?label_override=" + drug.drugName.toLowerCase().split(" ")[0];
-    let form = "&form=" + drug.drugForm.toLowerCase().replace(/\ +/g, "-");
-    let dosage = "&dosage=" + drug.dosageStrength.toLowerCase().replace(/\ m+/g, "m").replace(/\ +/g, "-");
-    let quantity = "&quantity=" + drug.quantity;
+async function fetchId(drug) {
+    const res = generateUrl(drug);
+    let response;
+    let model = res.model;
+    await fetch(res.url, {
+        method: 'get',
+        headers: {
+            "Connection": "keep-alive",
+            "host": "www.goodrx.com",
+            "Referer": "https://www.goodrx.com",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0"
+        },
+        retryOn: [403],
+        retryDelay: 30000,
+        retries: 2
+    }).then((response) => response.text())
+    .then((text) => extractId(text))
+    .then((id) => {
+        response = id;
+        console.log("Waiting 7.5s...")
+    })
+    .catch((error) => console.log(error));
 
-    return url + label_override + form + dosage + quantity;
+    return timeout(7500).then(() => {
+        model.goodRxId = response;
+        // console.log(model);
+        return model;
+    });
 }
 
 function extractId(text) {
     const start_index = text.indexOf("drug_id=") + 8;
     const end_index = start_index + text.substring(start_index).indexOf("&");
 
-    if (end_index - start_index < 15 ) {
+    if (end_index - start_index < 15) {
         let id = text.substring(start_index, end_index);
         // console.log(id);
         return id;
     } else {
         return null;
     }
-}
-
-async function getDrugId (drug) {
-    const url = generateUrl(drug);
-    const userAgent = userAgents[getRandomArbitrary(0, userAgents.length - 1)];
-    const referrer = referrers[getRandomArbitrary(0, referrers.length - 1)];
-    // console.log("Referrer: " + referrer, "UserAgent: "+ userAgent);
-
-    const response = await fetch(url, {
-        method: 'get',
-        headers: {
-            "User-Agent": userAgent,
-            "Accept": "*/*",
-            "Cache-Control": "no-cache",
-            "Referrer": referrer
-        }
-    });
-    const text = await response.text();
-    const id = extractId(text);
-    // if (id === null) {
-    //     // console.log("Referrer: " + referrer, "UserAgent: "+ userAgent);
-    // }
-    return id;
-}
-
-function getRandomArbitrary(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
 }
 
 test();
---
+++ genUrl.js
@@ -0,0 +1,422 @@
+function generateUrl(drug) {
+  const base = "https://www.goodrx.com/";
+  let packageDesc = drug.PACKAGE_DSC;
+  let quantity = ''
+  let form = ''
+  let dosage = ''
+  let name = '';
+  // console.log(packageDesc)
+  
+  switch (packageDesc) {
+    case "AER BR.ACT":
+      name = drug.BrandName.split(" ")[0].toLowerCase();
+      form = "redihaler";
+      dosage = "10.6g-of-" + drug.STRENGTH_MSR + drug.STRENGTH_UNIT_TXT.toLowerCase();
+      quantity = 1;
+      break;
+    case "AER W/ADAP":
+      switch (drug.BrandName) {
+        case "AIRDUO RESPICLICK":
+          name = drug.BrandName.split(" ")[0].toLowerCase();
+          form = drug.BrandName.split(" ")[1].toLowerCase();
+          dosage = "0.45g-of-" + 
+                drug.Strength.split("-")[0] + "mcg-" +
+                drug.Strength.split("-")[1].toLowerCase();
+          quantity = 1;
+          break;
+        case "COMBIVENT RESPIMAT":
+          name = drug.BrandName.split(" ")[0].toLowerCase();
+          form = drug.BrandName.split(" ")[1].toLowerCase().replace(" ", "") + "-inhaler";
+          dosage = "120-doses-of-" +
+                drug.Strength.split("-")[0].toLowerCase() + "mcg-" +
+                drug.Strength.split("-")[1].toLowerCase().replace(" ", "");
+          quantity = 1;
+          break;
+        case "FLOVENT HFA":
+          name = drug.BrandName.split(" ")[0].toLowerCase();
+          form = drug.BrandName.split(" ")[0].toLowerCase().replace(" ", "") + "-inhaler";
+          dosage = drug.STRENGTH_MSR + drug.STRENGTH_UNIT_TXT.toLowerCase();
+          quantity = 1;
+          break;
+        case "PROAIR RESPICLICK":
+          name = drug.BrandName.split(" ")[0].toLowerCase();
+          form = drug.BrandName.split(" ")[1].toLowerCase().replace(" ", "") + "-inhaler";
+          dosage = "8.5g-of-" + drug.STRENGTH_MSR + drug.STRENGTH_UNIT_TXT.toLowerCase();
+          quantity = 1;
+          break;
+        case "QVAR":
+          name = "QVAR";
+          form = "redihaler";
+          dosage = "10.6g-of-" + drug.STRENGTH_MSR + drug.STRENGTH_UNIT_TXT.toLowerCase();
+          quantity = 1;
+          break;
+        case "SPIRIVA RESPIMAT":
+          name = drug.BrandName.split(" ")[0].toLowerCase();
+          form = drug.BrandName.split(" ")[1].toLowerCase().replace(" ", "") + "-inhaler";
+          dosage = "60-doses-of-" + drug.STRENGTH_MSR + + drug.STRENGTH_UNIT_TXT.toLowerCase() + "-per-actuation"
+          quantity = 1;
+          break;
+        case "STIOLTO RESPIMAT":
+          name = drug.BrandName.split(" ")[0].toLowerCase();
+          form = drug.BrandName.split(" ")[1].toLowerCase().replace(" ", "") + "-inhaler";
+          dosage = "60-inhalations-of-" + drug.Strength.split("-")[1].toLowerCase();
+          quantity = 1;
+          break;
+        case "STRIVERDI RESPIMAT":
+          name = drug.BrandName.split(" ")[0].toLowerCase();
+          form = drug.BrandName.split(" ")[1].toLowerCase().replace(" ", "") + "-inhaler";
+          dosage = "60-doses-of-" + drug.STRENGTH_MSR + + drug.STRENGTH_UNIT_TXT.toLowerCase() + "-actuation";
+          quantity = 1;
+          break;
+        case "SYMBICORT":
+          name = drug.BrandName.toLowerCase();
+          form = "inhaler";
+          dosage = "120-doses-of-" + drug.Strength.split("-")[0] + "mcg-" + drug.Strength.split("-")[1].toLowerCase();
+          quantity = 1;
+          break;
+        case "XHANCE":
+          name = drug.BrandName.toLowerCase();
+          form = "nasal-spray";
+          dosage = "16ml-of-" + drug.Strength.replace(" ", "").toLowerCase();
+          quantity = 1;
+          break;
+      } break;
+    case "BLIST PACK":
+      name = drug.BrandName.replace(" ", "-").toLowerCase();
+      quantity = 1;
+      const subform = drug.DrugName.split(" ")[drug.DrugName.split(" ").length - 1];
+      // if (drug.BrandName === "GRASTEK") {
+      //   console.log(subform);
+      // }
+      if (subform === "CAPSULE") {
+        form = "capsule";
+        dosage = drug.STRENGTH_MSR + drug.STRENGTH_UNIT_TXT.toLowerCase();
+      } else if (subform === "INH") {
+        form = "inhaler";
+        dosage = drug.PACKAGE_SIZE_QTY + `-blisters-of-${
+          (drug.Strength.includes("-")) 
+            ? drug.Strength.split("-")[0] + "mcg-" + drug.Strength.split("-")[1].toLowerCase()
+            : drug.STRENGTH_MSR + drug.STRENGTH_UNIT_TXT.toLowerCase()
+          }`;
+      } else if (subform === "TABLET" ) {
+        form = "package";
+        name = drug.BrandName.replace(" ", "-").toLowerCase();
+        if (drug.Strength.includes("(")) {
+          dosage = drug.PACKAGE_SIZE_QTY + "-tablets";
+          quantity = 1;
+        } else if (drug.Strength.includes("-") && !drug.Strength.includes("(")) {
+          dosage = drug.Strength.toLowerCase();
+          quantity = drug.PACKAGE_SIZE_QTY;
+        } else {
+          dosage = drug.PACKAGE_SIZE_QTY + "-sublingual-tablets-of-" + drug.Strength.toLowerCase().replace(" ", "-");
+          quantity = 1;
+        }
+      } else if (subform === "PACK") {
+        let dArr = drug.Strength.split("-");
+        form = "kit";
+        dosage = "30-day-of-" + dArr[0] + "mg-" + dArr[1] + "mg-" + dArr[2] + "mg";
+        quantity = 1;
+      } else if (subform === "TAB") {
+        form = "dose-pack";
+        dosage = "30-sublingual-tablets-of-" + drug.Strength.replace(" ", "-").toLowerCase();
+        quantity = 1;
+      } else if (subform ==="DOSE)") {
+        form = "dose-pack";
+        dosage = "2-tablets-of-" + drug.Strength.replace(" ", "").toLowerCase();
+        quantity = 1;
+      } else if (subform === "SOLUTION") {
+        form = "box";
+        dosage = drug.PACKAGE_SIZE_QTY + "-vials-of-" + drug.Strength.replace("%", "%25");
+        quantity = 1;
+      } else if (subform === "DISKUS") {
+        form = subform.toLowerCase() + "-inhaler";
+        dosage = drug.STRENGTH_MSR + drug.STRENGTH_UNIT_TXT.toLowerCase();
+        quantity = 1;
+      } break;
+    case "BOTTLE":
+      name = drug.BrandName.replace(" ", "-").toLowerCase();
+      quantity = 1;
+      let end = drug.BrandName.split(" ")[drug.BrandName.split(" ").length - 1];
+      
+      if (end === "LOTION" || end === "SHAMPOO") {
+        form = "bottle-of-" + end.toLowerCase();
+        dosage = drug.PACKAGE_SIZE_QTY + "ml-of-" + drug.Strength.replace("%", "%25")
+      } else if (end.includes("CAP")) {
+        form = "capsule";
+        if (drug.Strength.includes("-")) {
+          let dArr = drug.Strength.split("-");
+          dosage = dArr[0] + "mg-" + dArr[1] + "mg-" + dArr[2] + "mg";
+        } else {
+          dosage = drug.STRENGTH_MSR + drug.STRENGTH_UNIT_TXT.toLowerCase();
+        }
+      } else if (end === "TABLET") {
+        form = "tablet";
+        if (drug.Strength.includes("-")) {
+          dosage = drug.Strength.replace(" ", "").toLowerCase(); 
+        } else {
+          dosage = drug.STRENGTH_MSR + drug.STRENGTH_MSR.toLowerCase();
+        }
+      } else if (end === "PUMP") {
+        form = "gel-pump";
+        if (drug.Strength.includes("-")) {
+          let dArr = drug.Strength.replace(" ", "").split("-");
+          dosage = drug.PACKAGE_SIZE_QTY + "-of-" + dArr[0].replace("%", "%25") + "-" + dArr[1].replace("%", "%25");
+        } else {
+          dosage =  drug.PACKAGE_SIZE_QTY + "-of-" + dArr[0].replace("%", "%25");
+        }
+      } else if (end === "SPRAY") {
+        form = "bottle-of-spray";
+        dosage =  drug.PACKAGE_SIZE_QTY + "ml-of-" + dArr[0].replace("%", "%25");
+      } else if (end === "INFATAB") {
+        form = "chewable-tablet";
+        dosage = drug.Strength.replace(" ", "").toLowerCase();
+      } else if (end === "SUSP" || end === "SUSPENSION") {
+        if (name.includes('dyanavel')) {
+          form = "oral-suspension"
+        } else {
+          form = "bottle-of-oral-suspension"
+        }
+        dosage = drug.PACKAGE_SIZE_QTY + "ml-of-" + drug.STRENGTH_MSR + "mg-" + drug.VOLUME_MSR + "ml";
+      } else if(end === "CONC") {
+        form = "bottle-of-oral-solution";
+        dosage = drug.PACKAGE_SIZE_QTY + "ml-of-" + drug.Strength.toLowerCase().replace("/", "-");
+      } else if (end === "GEL") {
+        let percent = drug.BrandName.split(" ")[drug.BrandName.split(" ").length - 2];
+        form = "gel-pump";
+        dosage = drug.PACKAGE_SIZE_QTY + "g-of-" + percent.replace("%", "%25");
+      } else if (end === "MG" || end === "TB") {
+        form = "tablet";
+        dosage = drug.Strength.toLowerCase();
+        quantity = drug.PACKAGE_SIZE_QTY;
+      } else if (end === "0.75%") {
+        form = "bottle-of-lotion";
+        dosage = drug.PACKAGE_SIZE_QTY + "ml-of-" + drug.Strength + "25";
+      } break;
+    case "BOX":
+      let nameSplit = drug.BrandName.split(" ");
+      name = nameSplit[0];
+      quantity = 1;
+
+      if (nameSplit.length > 1) {
+        if (nameSplit[1] === "FASTCLIX") {
+          form = "lancing-device";
+          dosage = "fastclix";
+        } else if (nameSplit[1] === "GUIDE") {
+          form = "test-strip";
+          dosage = "guide";
+          quantity = drug.PACKAGE_SIZE_QTY;
+        } else if (nameSplit[1] === "ULTRA") {
+          form = "test-strip";
+          dosage = "ultra-blue";
+          quantity = drug.PACKAGE_SIZE_QTY;
+        } else {
+          form = "kit";
+          dosage = drug.PACKAGE_SIZE_QTY + "-units";
+        }
+      } else {
+        if (drug.Strength.includes("%")) {
+          form = "patch";
+          dosage = drug.Strength.replace("0", "") + "25";
+          quantity = drug.PACKAGE_SIZE_QTY;
+        } else {
+          form = "carton";
+          dosage = drug.PACKAGE_SIZE_QTY + "-once-weekly-patches-of-" + drug.STRENGTH_MSR + drug.STRENGTH_UNIT_TXT.replace("/24h", "-day").toLowerCase;
+        }
+      } break;
+    case "CANISTER":
+      let nameSplit1 = drug.BrandName.split(" ");
+      name = nameSplit1[0].toLowerCase;
+      quantity = 1;
+
+      if (nameSplit1.length > 1) {
+        form = nameSplit1[1].toLowerCase;
+        if (form === "children") {
+          form = "inhaler";
+        } else if (form === "hfa") {
+          form = "hfa-inhaler";
+        }
+        if (drug.Strength.includes("-")) {
+          let dArr = drug.Strength.split("-");
+          dosage = dArr[0] + "mcg-" + dArr[1].toLowerCase();
+        } else {
+          dosage = drug.PACKAGE_SIZE_QTY + "g-of-" + drug.Strength.replace(" ", "").toLowerCase();
+        }
+      } else {
+        form = "inhaler";
+        dosage = drug.PACKAGE_SIZE_QTY + "g-of-" + drug.Strength.replace(" ", "").toLowerCase();
+      } break;
+    case "CARTRIDGE":
+      name = drug.BrandName.toLowerCase();
+      let psq = drug.PACKAGE_SIZE_QTY;
+      quantity = 1;
+
+      if (psq === 1) {
+        if (drug.Strength.includes("/")) {
+          dosage = drug.STRENGTH_MSR + drug.STRENGTH_UNIT_TXT.toLowerCase();
+          form = "cartridge";
+        } else {
+          dosage = "1-cartridge-of-" + drug.Strength.toLowerCase().replace(" ", "");
+          form = "kit";
+        }
+      } else {
+        dosage = psq + "-miniquick-devices-of-" + drug.STRENGTH_MSR + drug.STRENGTH_UNIT_TXT.toLowerCase();
+        form = "package";
+      } break;
+    case "DROP BTL":
+      name = drug.BrandName.toLowerCase();
+      quantity = 1;
+      form = "eye-dropper";
+      dosage = drug.PACKAGE_SIZE_QTY + "ml-of-" + drug.Strength + "25";
+      break;
+    case "JAR":
+      name = drug.BrandName.toLowerCase();
+      form = "jar-of-gel";
+      dosage = drug.PACKAGE_SIZE_QTY + "g-of-" + drug.Strength.replace(/[1-5]%/, "%25");
+      quantity = 1;
+      break;
+    case "KIT":
+      let drugSplit = drug.DrugName.split(" ");
+      name = drug.BrandName.replace("-");
+      form = "kit";
+      quantity = 1;
+      dosage = "50g-of-" + drug.Strength.split("-")[0] + `25-${drugSplit[2].toLowerCase}`;
+      break;
+    case "PF APPLI":
+      let nameArr = drug.BrandName.toLowerCase().split(" ");
+      name = nameArr[0];
+      form = nameArr[2];
+      dosage = drug.PACKAGE_SIZE_QTY + "ml-of-" + drug.STRENGTH_MSR + "-units-ml";
+      quantity = 1;
+      break;
+    case "PACKET":
+      name = drug.BrandName.toLowerCase().replace(" ", "-").replace("#", "-");
+      quantity = drug.PACKAGE_SIZE_QTY;
+      form = "carton";
+      dosage = drug.PACKAGE_SIZE_QTY + "-packets-of-" + drug.STRENGTH_MSR + `-${drug.STRENGTH_UNIT_TXT.toLowerCase().replace(" ", "-")}`;
+      break;
+    case "SYRINGE":
+      name = drug.BrandName.toLowerCase().replace(" ", "-");
+      let nameArr3 = drug.BrandName.toLowerCase().split(" ");
+      let formIndex = nameArr3.indexOf("kwikpen");
+      dosage = `${drug.PACKAGE_SIZE_QTY}ml-of-${drug.STRENGTH_MSR}-${drug.STRENGTH_UNIT_TXT === "UNIT" ? 'units' : drug.STRENGTH_UNIT_TXT.toLowerCase()}-${drug.VOLUME_UNIT_TXT}`
+
+      if (formIndex !== -1) {
+        form = nameArr3[formIndex];
+        quantity = 5;
+      } else if (nameArr3.length > 1) {
+        if (nameArr3[0] === "bydureon") {
+          name = nameArr3[0];  
+          quantity = 1;
+          if (nameArr3[1] === "bcise") {
+            form = "carton";
+            dosage = `4-pens-of-${drug.Strength.toLowerCase().replace("/", "-")}`
+          } else {
+            form = "kit";
+            dosage = `4-pens-of-${drug.STRENGTH_MSR}${drug.STRENGTH_UNIT_TXT.toLowerCase()}-pen`;
+          }
+        } else if (nameArr3[0] === 'emgality') {
+          form = nameArr3[1];
+          dosage = drug.Strength.replace(" ", "-").replace("/", "-").toLowerCase();
+          quantity = 1;
+        } else if (nameArr3[0] === 'humulin') {
+          name = `humulin-${nameArr3[1].replace("/", "-").toLowerCase()}`;
+          form = nameArr3[2];
+          quantity = 5;
+          dosage = `${drug.PACKAGE_SIZE_QTY}ml-of-${drug.STRENGTH_MSR}-units-${drug.VOLUME_UNIT_TXT.toLowerCase()}`
+        }
+      } else {
+        if (name === 'symjepi') {
+          quantity = 1;
+          form = 'package';
+          dosage = `${drug.PACKAGE_SIZE_QTY}-pre-filled-syringes-of-${drug.STRENGTH_MSR}${drug.STRENGTH_UNIT_TXT.toLowerCase()}`
+        } else {
+          quantity = drug.PACKAGE_SIZE_QTY;
+          form = 'carton';
+          dosage = `4-pens-of-${drug.Strength.replace("/", "-").toLowerCase()}ml`
+        }
+      }
+      break;
+    case "TUBE":
+      name = drug.BrandName.toLowerCase().replace(" ", "-");
+      let dArr = drug.DrugName.split(" ");
+      
+      if (dArr[dArr.length - 1] !== "KIT") {
+        // form = "";
+        form = `tube-of-${dArr[dArr.length - 1]}`;
+        dosage = `${drug.PACKAGE_SIZE_QTY}g-of-${drug.Strength}`; 
+        quantity = 1;
+      }
+      break;
+    case "TUBE/KIT":
+      name = drug.BrandName.toLowerCase();
+      form = "tube-of-cream";
+      quantity = 1;
+      dosage = drug.PACKAGE_SIZE_QTY + "g-of-" + drug.Strength.toLowerCase().replace(" ", "").replace("/", "-");
+      break;
+    case "VIAL":
+      let nameArr2 = drug.BrandName.toLowerCase().split(" ");
+      form = "vial";
+      quantity = 1;
+
+      if (nameArr2.length > 1) {
+        name = nameArr2[0] + "-" + nameArr2[1];
+      } else if (nameArr2.length > 2) {
+        name = nameArr2[0] + "-" + nameArr2[2];
+        dosage = drug.PACKAGE_SIZE_QTY + "ml-of-" + drug.STRENGTH_MSR + "-units-ml";
+      } else {
+        name = nameArr2[0];
+        if (drug.STRENGTH_UNIT_TXT === "UNIT") {
+          dosage = drug.PACKAGE_SIZE_QTY + "ml-of-" + drug.STRENGTH_MSR + "-units-ml";
+        } else if (name === "humatrope") {
+          form = "kit";
+          dosage = drug.PACKAGE_SIZE_QTY + `-${name === "humatrope" ? 'cartridge' : 'pens'}-of-` + drug.Strength.toLowerCase().replace(" ", "-") + `${name === "humatrope" ? "" : "-pen"}`
+        }
+      }
+      break;
+    default:
+      break;
+  }
+  
+  
+  let url = base + name;
+  let formParam;
+  if (drug.drugForm !== "") {
+    formParam = "?form=" + form + "&";
+  } else {
+    formParam = "?";
+  }
+  let dosageParam = "dosage=" + dosage;
+  let quantityParam = "&quantity=" + quantity;
+  
+  let response = url + formParam + dosageParam + quantityParam;
+  const model = {
+    drugName: name.replace(/^\w/, c => c.toUpperCase()),
+    drugForm: form,
+    dosageStrength: drug.Strength,
+    dosageStrengthNum: drug.STRENGTH_MSR,
+    dosageStrengthUnit: drug.STRENGTH_UNIT_TXT,
+    volumeNum: drug.VOLUME_MSR,
+    volumeUnit: drug.VOLUME_UNIT_TXT,
+    quantity: quantity,
+    gsn: '' + drug.GSN,
+    ndc: '' + drug.NDC11,
+    goodRxId: '',
+    url: response
+  };
+  if (name === "" || form === "" || dosage === "" || quantity === "") {
+    console.log("ERROR: undefined value");
+    console.log({name: name, form: form, dosage: dosage, quantity: quantity});
+    return {
+      url: response,
+      model: {},
+      error: "ERROR: undefined value"
+    }
+  } else {
+    return {
+      url: response,
+      model: model
+    }
+  }
+}
+
+module.exports = generateUrl;
